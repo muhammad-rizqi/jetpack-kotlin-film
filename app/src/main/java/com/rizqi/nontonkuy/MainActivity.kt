83c6b2618b7681e1ad00f85829d3e4d5a6da3006
Muhammad Rizqi <muhammad-rizqi@live.com>
2020-12-12 12:55:01 +0700

    add navigating to favorite activity

--- app/src/main/java/com/rizqi/nontonkuy/MainActivity.kt
+++ app/src/main/java/com/rizqi/nontonkuy/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.rizqi.nontonkuy
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import com.rizqi.nontonkuy.ui.SectionsPagerAdapter
 import kotlinx.android.synthetic.main.activity_main.*
@@ -17,5 +20,23 @@ class MainActivity : AppCompatActivity() {
     view_pager.adapter = sectionsPagerAdapter
     tabs.setupWithViewPager(view_pager)
     supportActionBar?.elevation = 0f
+  }
+
+  override fun onCreateOptionsMenu(menu: Menu): Boolean {
+    val inflater = menuInflater
+    inflater.inflate(R.menu.options_menu, menu)
+    return true
+  }
+
+  override fun onOptionsItemSelected(item: MenuItem): Boolean {
+    return when (item.itemId) {
+      R.id.menu_favorite -> {
+        val i = Intent(this, FavoriteActivity::class.java)
+        startActivity(i)
+        true
+      }
+
+      else -> true
+    }
   }
 }
