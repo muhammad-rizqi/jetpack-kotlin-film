1c08741243ece62ce3edf41873893051e34269e7
Muhammad Rizqi <muhammad-rizqi@live.com>
2020-12-13 09:55:20 +0700

    add entity and dependency

--- app/src/main/java/com/rizqi/nontonkuy/data/model/Movie.kt
+++ app/src/main/java/com/rizqi/nontonkuy/data/model/Movie.kt
@@ -1,14 +1,30 @@
 package com.rizqi.nontonkuy.data.model
 
 import android.os.Parcelable
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import kotlinx.android.parcel.Parcelize
 
+@Entity(tableName = "movies")
 @Parcelize
 data class Movie (
-    var id: Int,
-    var poster_path: String,
-    var original_title: String,
-    var vote_average: Double,
-    var overview: String,
-    var release_date: String,
+  @PrimaryKey
+  @ColumnInfo(name = "id")
+  var id: Int,
+
+  @ColumnInfo(name = "poster_path")
+  var poster_path: String,
+
+  @ColumnInfo(name = "original_title")
+  var original_title: String,
+
+  @ColumnInfo(name = "vote_average")
+  var vote_average: Double,
+
+  @ColumnInfo(name = "overview")
+  var overview: String,
+
+  @ColumnInfo(name = "release_date")
+  var release_date: String,
 ): Parcelable
--- app/src/main/java/com/rizqi/nontonkuy/data/model/Tv.kt
+++ app/src/main/java/com/rizqi/nontonkuy/data/model/Tv.kt
@@ -1,17 +1,37 @@
 package com.rizqi.nontonkuy.data.model
 
 import android.os.Parcelable
+import androidx.room.ColumnInfo
+import androidx.room.Entity
 import kotlinx.android.parcel.Parcelize
 
+@Entity(tableName = "tvs")
 @Parcelize
 data class Tv (
+    @ColumnInfo(name = "id")
     val id : Int,
+
+    @ColumnInfo(name = "name")
     val name : String,
+
+    @ColumnInfo(name = "popularity")
     val popularity : Double,
+
+    @ColumnInfo(name = "vote_count")
     val vote_count : Int,
+
+    @ColumnInfo(name = "first_air_date")
     val first_air_date : String,
+
+    @ColumnInfo(name = "backdrop_path")
     val backdrop_path : String,
+
+    @ColumnInfo(name = "vote_average")
     val vote_average : Double,
+
+    @ColumnInfo(name = "overview")
     val overview : String,
+
+    @ColumnInfo(name = "poster_path")
     val poster_path : String
 ): Parcelable
